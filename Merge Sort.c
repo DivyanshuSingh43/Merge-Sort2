0c2fcb3ed83cae859e09b2943543df2388246389
DivyanshuSingh43 <singhdivyanhu0408@gmail.com>
2026-01-14 16:31:41 +0530

    First Commit

---
+++ Merge Sort.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+// Merge function
+void merge(int a[], int l, int m, int r) {
+    int i = l, j = m + 1, k = 0;
+    int temp[50];
+
+    while (i <= m && j <= r) {
+        if (a[i] < a[j])
+            temp[k++] = a[i++];
+        else
+            temp[k++] = a[j++];
+    }
+
+    while (i <= m)
+        temp[k++] = a[i++];
+
+    while (j <= r)
+        temp[k++] = a[j++];
+
+    for (i = l, k = 0; i <= r; i++, k++)
+        a[i] = temp[k];
+}
+
+// Merge Sort function
+void mergeSort(int a[], int l, int r) {
+    int m;
+    if (l < r) {
+        m = (l + r) / 2;
+        mergeSort(a, l, m);
+        mergeSort(a, m + 1, r);
+        merge(a, l, m, r);
+    }
+}
+
+// Main function
+int main() {
+  int a[50],n,i;
+  printf("Enter no. of elements:");
+  scanf("%d",&n);
+  printf("Enter array element:");
+  for(i=0;i<n;i++)
+  {
+      scanf("%d",&a[i]);
+  }
+    mergeSort(a, 0, n-1);
+
+    printf("Sorted Array:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
